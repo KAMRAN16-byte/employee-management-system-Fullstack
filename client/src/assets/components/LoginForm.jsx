915d658efa6fccbc35169ea01d3b16493062d983
KAMRAN16-byte <mdkamran_16@outlook.com>
2026-07-15 16:33:10 +0530

    Update

--- client/src/assets/components/LoginForm.jsx
+++ client/src/assets/components/LoginForm.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Logintemplate from "./Logintemplate.jsx";
+import {Link} from "react-router-dom";
+import {ArrowLeftIcon} from "lucide-react";
 
-const LoginForm = () => {
+const LoginForm = ({role,title,subtitle}) => {
   return (
-    <div className=''>
-      Login Form
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <Logintemplate />
+        <div className='flex-1 flex items-center justify-center'>
+            <div className="w-full max-w-md animate-fade-in">
+                <Link to='/login' className={'group inline-flex items-center gap-2 text-slate-400 hover:text-slate-700 text-sm mb-10 transition-colors'}>
+                    <ArrowLeftIcon className={"transition-transform group-hover:-translate-x-1"} size={20} /> Back to Portals
+                </Link>
+
+                <div className={'mb-8'}>
+                    <h1 className={'text-2xl sm:text-3xl font-medium text-zinc-800'}>{title}</h1>
+                    <p className={'text-slate-500  text-sm sm:text-base mt-2'}>{subtitle}</p>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
