0fd20d449d5cde170c60823e98b9cf386980d1fd
KAMRAN16-byte <mdkamran_16@outlook.com>
2026-07-21 02:00:08 +0530

    Layout Modified

--- client/src/assets/pages/Layout.jsx
+++ client/src/assets/pages/Layout.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Sidebar from "../components/Sidebar.jsx";
 
 const Layout = () => {
   return (
     <div className='flex h-screen bg-linear-to-br from-slate-50 via-white to-indigo-50/30'>
-        <div className={"ml-20 lg:ml-0"}>
         <Sidebar />
+        <div className={"ml-20 lg:ml-0"}>
+
         </div>
       <main className='flex-1 overflow-y-auto'>
         <div className='p-5 pt-16 sm:p-6 sm:pt-6 lg:p-8 max-w-400 mx-auto'>
