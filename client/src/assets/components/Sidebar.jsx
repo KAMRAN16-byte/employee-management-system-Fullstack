129e708799d35d95793ab072f1bf2790c8d6e84d
KAMRAN16-byte <mdkamran_16@outlook.com>
2026-07-20 20:21:16 +0530

    Menu - Button UI Modified

--- client/src/assets/components/Sidebar.jsx
+++ client/src/assets/components/Sidebar.jsx
@@ -21,15 +21,38 @@ const Sidebar = () => {
         setMobileOpen(value);
     }
 
+    const sidebarContent = (
+        <>
+            {/* Brand Header */}
+
+        </>
+    );
+
     return (
         <>
             {/* Mobile Menu Button*/}
-            <button onClick={() => toggleMenu(!mobileOpen)} className={'lg:hidden fixed top-4 left-4 z-50 p-2 bg-black/15 text-black rounded-lg shadow-lg border border-white/20 active:scale-85 transition-transform duration-150'}>
-                {mobileOpen ? <X size={20}/> : <MenuIcon size={20}/>}
+            <button onClick={() => toggleMenu(!mobileOpen)} className={`lg:hidden fixed top-4 left-4 z-60 p-2 ${mobileOpen ? 'bg-white/80' : 'bg-black/15'} backdrop-blur-md text-black rounded-lg shadow-lg border border-white/20 active:scale-85 transition-all duration-200`}>
+                    {mobileOpen ? <X size={20}/> : <MenuIcon size={20}/>}
             </button>
 
+
             {/* Mobile Overlay */}
-            {mobileOpen && <div className={'lg:hidden fixed inset-0 bg-black/20 backdrop-blur-[2px] z-40'} onClick={ () => toggleMenu(false)} />}
+            {mobileOpen && <div className={'lg:hidden fixed inset-0 bg-black/20 backdrop-blur-[2px] z-40 animate-fade-in'} onClick={ () => toggleMenu(false)} />}
+
+            {/* Sidebar - desktop */}
+            <aside className={"hidden lg:flex flex-col h-full w-65 bg-linear-to-b from-slate-900 via-slate-600 to-slate-950 text-white shrink-0 border-r border-white/4"}>
+                {sidebarContent}
+            </aside>
+
+            {/* Sidebar - Mobile */}
+            <aside className={`lg:hidden fixed inset-y-0 left-0 w-72 bg-linear-to-b from-slate-900 via-slate-900 to-slate-950 text-white z-50 flex flex-col transform transition-transform
+            duration-300 ${mobileOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+                {sidebarContent}
+            </aside>
+
+
+
+
 
         </>
     );
--- client/src/assets/pages/Layout.jsx
+++ client/src/assets/pages/Layout.jsx
@@ -5,10 +5,11 @@ import Sidebar from "../components/Sidebar.jsx";
 const Layout = () => {
   return (
     <div className='flex h-screen bg-linear-to-br from-slate-50 via-white to-indigo-50/30'>
-      <p>Sidebar</p>
+        <div className={"ml-20 lg:ml-0"}>
+        <Sidebar />
+        </div>
       <main className='flex-1 overflow-y-auto'>
         <div className='p-5 pt-16 sm:p-6 sm:pt-6 lg:p-8 max-w-400 mx-auto'>
-            <Sidebar />
           <Outlet />
         </div>
       </main>
