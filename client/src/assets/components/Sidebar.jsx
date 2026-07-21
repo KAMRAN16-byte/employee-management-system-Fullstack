27f8938a261b97b877caa961adbbd05fba53fc58
KAMRAN16-byte <mdkamran_16@outlook.com>
2026-07-22 03:04:33 +0530

    Sidebar UI - Navigation and Icons added with stylish UI.

--- client/src/assets/components/Sidebar.jsx
+++ client/src/assets/components/Sidebar.jsx
@@ -19,6 +19,7 @@ const Sidebar = () => {
 
     },[])
     useEffect(() => {
+        // eslint-disable-next-line react-hooks/set-state-in-effect
         setMobileOpen(false);
     }, [pathname]);
 
