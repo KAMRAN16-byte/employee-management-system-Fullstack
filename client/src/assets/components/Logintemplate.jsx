db4255d94c9daf179fe165eb393ec1cff86b97fa
KAMRAN16-byte <mdkamran_16@outlook.com>
2026-07-23 01:47:57 +0530

    Login UI Updated.

--- client/src/assets/components/Logintemplate.jsx
+++ client/src/assets/components/Logintemplate.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
+import {assets} from "../assets.jsx";
 
 const Logintemplate = () => {
     return (
-        <div className='hidden md:flex w-1/2 bg-[#0B090A] relative overflow-hidden border-r border-slate-200'>
-            <div className="absolute -top-30 -left-30 w-72 h-72 bg-[#BA181B]/20 rounded-full blur-3xl"></div>
-            <div className="absolute top-60 left-30 lg:left-120 w-72 h-72 bg-[#BA181B]/20 rounded-full blur-3xl"></div>
-            <div className="relative z-10 flex flex-col items-start justify-center p-12 lg:p-20 w-full h-full">
-                <h1 className="text-4xl lg:text-5xl font-medium text-white mb-6 leading-tight tracking-tight">Employee <br /> Management System</h1>
-                <p className="text-slate-400 text-lg max-w-lg leading-relaxed w-full">
-                    Streamline your workforce management with our comprehensive Employee Management System. From attendance tracking to payroll processing, our platform simplifies HR tasks, enhances productivity, and empowers your team to focus on what matters most. Experience seamless employee management today.
-                </p>
+        <div className={'md:flex w-1/2 '}>
+            <div className={'z-10 absolute '}>
+                <img src={assets.BackgroundImage2} className={' min-h-screen  w-full md:w-1/2  object-fill'} />
             </div>
+            <div className='hidden md:flex bg-[#0B090A] relative overflow-hidden border-r border-slate-200'>
+
+                <div className="relative z-20 flex flex-col items-start justify-center p-12 lg:p-20 w-full h-full">
+                    <h1 className="text-4xl lg:text-5xl font-medium text-white mb-6 leading-tight tracking-tight">Employee <br /> Management System</h1>
+                    <p className=" bg-primary-950/30 backdrop-blur-xl   text-slate-100 text-lg max-w-lg leading-relaxed w-full pl-2">
+                        Streamline your workforce management with our comprehensive Employee Management System. From attendance tracking to payroll processing, our platform simplifies HR tasks, enhances productivity, and empowers your team to focus on what matters most. Experience seamless employee management today.
+                    </p>
+                </div>
 
 
+            </div>
         </div>
+
     )
 }
 
