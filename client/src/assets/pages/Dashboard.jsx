ad45b5285a8065efe6f961cd109d73d1618a6de7
KAMRAN16-byte <mdkamran_16@outlook.com>
2026-07-27 04:09:19 +0530

    Dashboard - AdminDashboard.jsx Added.

--- client/src/assets/pages/Dashboard.jsx
+++ client/src/assets/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import {assets, dummyAdminDashboardData, dummyEmployeeDashboardData} from "../as
 import {useEffect, useState} from "react";
 import Loading from "../components/Loading.jsx";
 import EmployeeDashboard from "../components/EmployeeDashboard.jsx";
+import AdminDashboard from "../components/AdminDashboard.jsx";
 
 const Dashboard = () => {
     const [data,setData] = useState(null);
@@ -18,17 +19,8 @@ const Dashboard = () => {
     if(loading) return (<Loading />);
     if(data.role === "ADMIN") return (
         <div className={'flex-1 flex flex-col h-full'}>
-            <div className={'p-4 mb-5 flex-1 border'}>
-                <h2 className={'text-2xl font-semibold text-slate-900 '}>
-                    Dashboard
-                </h2>
-                <p>
-                    Manage your system
-                </p>
-                <p>Hello Admin!</p>
-            </div>
+            <AdminDashboard data={data} />
         </div>
-
     )
     else return (
         <div className={'flex-1 flex flex-col h-full'}>
