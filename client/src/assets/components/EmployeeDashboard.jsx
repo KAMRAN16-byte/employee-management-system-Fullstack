b76e792e1acee7103db2df610fa6527f57cc6d3b
KAMRAN16-byte <mdkamran_16@outlook.com>
2026-07-26 06:40:12 +0530

    Dashboard Update - EmployeeDashboard.jsx Added

---
+++ client/src/assets/components/EmployeeDashboard.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import {assets} from "../assets.jsx";
+
+const EmployeeDashboard = ({data}) => {
+    const emp = data.employee;
+
+    const cards = [
+        {
+            icon:assets.EmployeeAttendanceIcon,
+            value: data.currentMonthAttendance,
+            title: "Days Present",
+            subtitle: "This month",
+        },
+        {
+            icon: assets.EmployeeLeaveIcon,
+            value: data.pendingLeaves,
+            title: "Pending Leaves",
+            subtitle: "Awaiting approval",
+        },
+        {
+            icon: assets.EmployeePayslipsIcon,
+            value: data.latestPayslip ? `$${data.latestPayslip.netSalary?.toLocaleString()}` : "N/A",
+            title: "latest Payslip",
+            subtitle: "Most recent payout",
+        },
+    ]
+    return (
+        <div className={'animate-fade-in'}>
+            <div className={'page-header'}>
+                <h1 className={'page-title'}>
+                        Welcome, {emp?.firstName}!
+                </h1>
+                <p className={'page-subtitle'}>
+                    {emp?.position} - {emp?.department}
+                </p>
+            </div>
+            <div className={'grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 sm:gap-5 mb-8'}>
+                {cards.map((card, index) => (
+                    <div key={index} className={'card card-hover p-5 sm:p-6 relative overflow-hidden group flex items-center justify-between '}>
+                        <div>
+                            <div className="absolute left-0 top-0 bottom-0 w-1 rounded-r-full overflow-hidden">
+                                <div className="absolute inset-0 bg-slate-500/70 transition-opacity duration-500 group-hover:opacity-0" />
+
+                                <div className="absolute inset-0 bg-linear-to-b from-[#ef2b16] via-slate-900 to-[#0f3d5b] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+                            </div>
+                            <p className={'text-sm font-medium md:text-lg text-slate-700'}>
+                                {card.title}
+                            </p>
+                            <p className={'text-2xl font-bold md:text-3xl text-slate-900 mt-1'}>
+                                {card.value}
+                            </p>
+                        </div>
+                        <div>
+                            <img src={card.icon} alt={card.subtitle} className={'w-25 h-25 lg:h-25 lg:w-25'} />
+                        </div>
+                    </div>
+                    )
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default EmployeeDashboard;
--- client/src/assets/pages/Dashboard.jsx
+++ client/src/assets/pages/Dashboard.jsx
@@ -1,10 +1,41 @@
+import {assets, dummyAdminDashboardData, dummyEmployeeDashboardData} from "../assets.jsx";
+import {useEffect, useState} from "react";
+import Loading from "../components/Loading.jsx";
+import EmployeeDashboard from "../components/EmployeeDashboard.jsx";
 
 const Dashboard = () => {
-  return (
-    <div>
-      Dashboard
-    </div>
-  ) 
+    const [data,setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        // eslint-disable-next-line react-hooks/set-state-in-effect
+        setData(dummyEmployeeDashboardData)
+        setLoading(true)
+        setTimeout(()=>{
+            setLoading(false);
+        },3000)
+    }, []);
+
+    if(loading) return (<Loading />);
+    if(data.role === "ADMIN") return (
+        <div className={'flex-1 flex flex-col h-full'}>
+            <div className={'p-4 mb-5 flex-1 border'}>
+                <h2 className={'text-2xl font-semibold text-slate-900 '}>
+                    Dashboard
+                </h2>
+                <p>
+                    Manage your system
+                </p>
+                <p>Hello Admin!</p>
+            </div>
+        </div>
+
+    )
+    else return (
+        <div className={'flex-1 flex flex-col h-full sm:ml-10 lg:ml-0'}>
+            <EmployeeDashboard data={data} />
+        </div>
+
+    )
 }
 
 export default Dashboard
