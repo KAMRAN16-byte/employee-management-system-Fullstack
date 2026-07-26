f7796423a3b9740378a309ca4168aab46ba6bb85
KAMRAN16-byte <mdkamran_16@outlook.com>
2026-07-26 06:30:41 +0530

    Login UI Updated - stylish page for Mobile.

--- client/src/assets/components/Logintemplate.jsx
+++ client/src/assets/components/Logintemplate.jsx
@@ -5,9 +5,9 @@ const Logintemplate = () => {
     return (
         <div className={'md:flex w-1/2 '}>
             <div className={'z-10 absolute '}>
-                <img src={assets.BackgroundImage2} className={' min-h-screen  w-full md:w-1/2  object-fill'} />
+                <img src={assets.BackgroundImage2} className={' min-h-screen  w-full md:w-1/2  object-fill'}  alt={'login background'}/>
             </div>
-            <div className='hidden md:flex bg-[#0B090A] relative overflow-hidden border-r border-slate-200'>
+            <div className='hidden md:flex  relative overflow-hidden border-r border-slate-200'>
 
                 <div className="relative z-20 flex flex-col items-start justify-center p-12 lg:p-20 w-full h-full">
                     <h1 className="text-4xl lg:text-5xl font-medium text-white mb-6 leading-tight tracking-tight">Employee <br /> Management System</h1>
@@ -15,8 +15,6 @@ const Logintemplate = () => {
                         Streamline your workforce management with our comprehensive Employee Management System. From attendance tracking to payroll processing, our platform simplifies HR tasks, enhances productivity, and empowers your team to focus on what matters most. Experience seamless employee management today.
                     </p>
                 </div>
-
-
             </div>
         </div>
 
